0769dbd8fe6564637c63010f450e68c534424918
jdebroux <jtdebroux@gmail.com>
2023-04-19 10:23:39 -1000

    updated file structure

--- src/main/java/org/theceshop/Driver.java
+++ src/main/java/org/theceshop/Driver.java
@@ -1,9 +1,9 @@
-package org.theceshop.main;
+package org.theceshop;
 
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
-public class Main {
+public class Driver {
     public static void main(String[] args) {
 
     }
